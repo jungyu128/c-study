e9dd7a36e43be9d9c590c187593d94c6e690409f
kangjungyu <kangjun449@gmail.com>
2026-03-30 15:48:46 +0900

    c언어실습

---
+++ array/2array-2.c
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS   
+#include <stdio.h>
+int main(void)
+{
+    char i;
+    char fruits[3][20];
+
+    for (i = 0; i < 3; i++)
+    {
+        printf("과일을 입력하세요: "); 
+        scanf_s("%s", fruits[i], (unsigned int)sizeof(fruits[i]));
+    }
+
+    printf("\n--- 입력한 과일 목록 ---\n");
+    for (i = 0; i < 3; i++) {
+        printf("%d번째 과일: %s\n", i + 1, fruits[i]);
+    }
+    return 0;
+}
---
+++ array/char-array-1.c
@@ -0,0 +1,16 @@
+﻿* 문자열 ppt -> 5번 7번 9번 슬라이드
+// 문자열의 길이를 구하는 프로그램
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+    char str[30] = "C language";
+    int i = 0;
+
+    while (str[i] != '\0')
+        i++;
+
+    printf("문자열\"%s\"의 길이는 %d입니다.\n", str, i);
+    return 0;
+}
