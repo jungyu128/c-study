acf0eff5cf1eb84fd7e183ab326ade2acef24f8d
kangjungyu <kangjun449@gmail.com>
2026-04-06 15:48:20 +0900

    포인터2

---
+++ pointer/double-pointer-2.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+//void change(int* p)
+//{
+//	int b = 20;
+//	p = &b;
+//}
+//int main(){
+//	int a = 10;
+//	int* p = &a;
+//	change(p);
+//	printf("%d\n", *p);
+//
+//
+//
+//return 0;
+//
+//
+//
+//}
+//
+
+void change(int** pp)
+{
+	int b = 20;
+	*pp = &b;
+}
+int main() {
+	int a = 10;
+	int** p = &a;
+	change(&p);
+	printf("%d\n", *p);
+
+
+
+	return 0;
+
+
+
+}
+
